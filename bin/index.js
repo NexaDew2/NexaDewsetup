8c46cfde4abae8bd5aaaa349ca12a87e720fb417
adityaujalambkar <104122039+adityaujalambkar@users.noreply.github.com>
2025-06-03 22:19:15 +0530

    all files are correctly setuped (#1)
    Co-authored-by: gitrunner-aju <adityajujalambkar@gmail.com>

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const setupPath = join(__dirname, '../setup.sh');
+
+execSync(`bash "${setupPath}"`, { stdio: 'inherit' });
